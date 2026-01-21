36b5c8d439af07ff6ad6702f2b8bcad5f59ff6b0
jacksonfishburn <jacksonfishburn@gmail.com>
2026-01-21 16:46:37 -0700

    RookMoveCalculator implemented

---
+++ shared/src/main/java/chess/RookMoveCalculator.java
@@ -0,0 +1,15 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMoveCalculator extends MoveCalculator {
+
+    public RookMoveCalculator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+    }
+
+    public Collection<ChessMove> moves() {
+        return straitMoves();
+    }
+}
