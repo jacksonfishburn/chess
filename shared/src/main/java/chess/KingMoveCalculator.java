9394d1e0987448fefbc9e82f6abf72171a779953
jacksonfishburn <jacksonfishburn@gmail.com>
2026-01-21 16:40:53 -0700

    KingMoveCalculator implemented

---
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -0,0 +1,31 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KingMoveCalculator extends MoveCalculator {
+
+    public KingMoveCalculator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+    }
+
+    public Collection<ChessMove> moves() {
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+
+        for (int i = -1; i < 2; i++){
+            for (int k = -1; k < 2; k++){
+                makeMove(possibleMoves, i, k);
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    private void makeMove(Collection<ChessMove> possibleMoves, int x, int y) {
+        if (x == 0 && y == 0) {
+            return;
+        }
+        ChessPosition pos = new ChessPosition(position.getRow() + x, position.getColumn() + y);
+        addMove(possibleMoves, pos);
+    }
+}
