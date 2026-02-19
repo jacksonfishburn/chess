048fc037655e74a5b641f7860e1bc9f56bbed3df
jacksonfishburn <jacksonfishburn@gmail.com>
2026-02-18 19:22:17 -0700

    GameDAO created

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,11 +2,11 @@ package dataaccess;
 
 import models.GameData;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface GameDAO {
     void createGame(String username, String gameName);
-    GameData getGame(String gameID);
-    List<GameData> listGames();
-    void updateGame(String gameID, String playerColor, String userName);
+    GameData getGame(int gameID);
+    Collection<GameData> listGames();
+    void updateGame(int gameID, String playerColor, String userName);
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,31 +1,46 @@
 package dataaccess;
 
+import chess.ChessGame;
 import models.GameData;
 
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
 
 public class MemoryGameDAO implements GameDAO {
-    Map<String, GameData> data = new HashMap<>();
+    Map<Integer, GameData> data = new HashMap<>();
+
+    int currentID = 0;
 
     @Override
     public void createGame(String username, String gameName) {
-
+        GameData game =  new GameData(currentID, username, null, gameName, new ChessGame());
+        data.put(currentID, game);
+        currentID++;
     }
 
     @Override
-    public GameData getGame(String gameID) {
-        return null;
+    public GameData getGame(int gameID) {
+        return data.get(gameID);
     }
 
     @Override
-    public List<GameData> listGames() {
-        return List.of();
+    public Collection<GameData> listGames() {
+        return data.values();
     }
 
     @Override
-    public void updateGame(String gameID, String playerColor, String userName) {
+    public void updateGame(int gameID, String playerColor, String userName) {
+        GameData game = data.get(gameID);
+        GameData newGame;
 
+        if (Objects.equals(playerColor, "white")) {
+            newGame = new GameData(gameID, userName, game.blackUserName(), game.gameName(), game.game());
+        } else {
+            newGame = new GameData(gameID, game.whiteUserName(), userName, game.gameName(), game.game());
+        }
+        data.put(gameID, newGame);
     }
 }
