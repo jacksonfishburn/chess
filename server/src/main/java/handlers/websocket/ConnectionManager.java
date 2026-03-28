68dd953da77d2683b972c1cd3be8d93c1be81019
jacksonfishburn <jacksonfishburn@gmail.com>
2026-03-28 14:04:38 -0600

    add broadcast to connection manager

--- server/src/main/java/handlers/websocket/ConnectionManager.java
+++ server/src/main/java/handlers/websocket/ConnectionManager.java
@@ -1,7 +1,10 @@
 package handlers.websocket;
 
+import json.JsonSerializer;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
@@ -13,5 +16,16 @@ public class ConnectionManager {
 
     public void remove(Session session) {
         connections.remove(session);
+    }
+
+    public void broadcast(Session excludeSession, ServerMessage message) throws IOException {
+        String msg = JsonSerializer.toJson(message);
+        for (Session c : connections.values()) {
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)) {
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
     }
 }
